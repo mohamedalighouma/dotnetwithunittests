f8392e0d8a8ea029f11acfb439fc4110534dcf46
Mohamed Ali GHOUMA <uid1354@focus.com.tn>
2021-09-09 17:06:42 +0100

    first commit

---
+++ unit-testing-using-dotnet-test/PrimeService.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace PrimeService.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+         
+
+            
+        }
+
+        
+    }
+}
